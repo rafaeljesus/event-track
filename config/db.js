13280f5670ac89ed00109300e4aa52fa266d6473
rafaeljesus <rafaelljesus86@gmail.com>
2016-03-06 10:54:17 -0300

    Change db name

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const host = process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost'
 const port = process.env.MONGO_PORT_27017_TCP_PORT || 27017
 
 export default {
-  test: `mongodb://${host}:${port}/track_test`,
-  development: `mongodb://${host}:${port}/track`,
+  test: `mongodb://${host}:${port}/events_test`,
+  development: `mongodb://${host}:${port}/events`,
   production: process.env.MONGODB_URL
 }
